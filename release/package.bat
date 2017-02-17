e576252586c0c0c9e7457447ec7d9036a5aff86e
darkdaddy <darkjadenkim@gmail.com>
2017-02-17 16:07:29 +0900

    0.1 released

---
+++ release/package.bat
@@ -0,0 +1,19 @@
+
+@SET VERSION=0.1
+@SET SOURCE_DIR=..\..\Lineage2RevBot
+@SET TARGET=Lineage2RevBot
+@SET TARGET_DIR=.\
+
+@SET ZIP_7Z_FILE=7z\7z.exe
+@SET ARCHIVE_FILE_NAME=%TARGET%_%VERSION%.zip
+
+rmdir /Q /S %TARGET_DIR%\%TARGET%
+mkdir %TARGET_DIR%\%TARGET%
+
+copy %SOURCE_DIR%\Lineage2Rev.exe %TARGET_DIR%\%TARGET%\ 
+
+::============================================
+:: Archive zip file..
+::============================================
+del /F/Q/S %ARCHIVE_FILE_NAME%
+%ZIP_7Z_FILE% a -tzip %TARGET_DIR%\%ARCHIVE_FILE_NAME% %TARGET_DIR%\%TARGET%
